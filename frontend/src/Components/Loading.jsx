bb95f8376d0f5addc8cb6e6cbe766a8286b7b572
221fa04732 <221fa04732@gmail.com>
2025-01-20 12:25:52 +0530

    loding page dynamic colour adde

--- frontend/src/Components/Loading.jsx
+++ frontend/src/Components/Loading.jsx
@@ -1,6 +1,12 @@
+import { useRecoilState } from 'recoil';
+import { LightDarkModeAtom } from '../Atoms/LightDarkModeAtom'
+
+
 export default function Loading()
 {
-    return(<div className="h-screen flex justify-center items-center flex-col sm:flex-row">
+    const [lightmode, setLightmode] = useRecoilState(LightDarkModeAtom)
+
+    return(<div className={`${lightmode === 1 ? "bg-stone-800" : "bg-gray-400"} h-screen flex justify-center items-center flex-col sm:flex-row`}>
                 <div className="w-12 h-12 border-4 border-white border-t-red-500 rounded-full animate-spin mr-5"></div>
             <div className="flex items-center">
                 <p className="font-bold text-lg">Loading...</p>
